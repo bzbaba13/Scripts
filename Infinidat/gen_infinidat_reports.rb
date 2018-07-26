73b8fd5d9751912ecf489d3fd6e6cf7e305c6014
FxM <fma@yp.com>
2018-07-26 16:01:22 -0700

    updated events report

--- Infinidat/gen_infinidat_reports.rb
+++ Infinidat/gen_infinidat_reports.rb
@@ -9,7 +9,6 @@
 # == Author
 # Friendly half-blind Lead Storage/Systems Administrator with smiles.
 #
-# $Id: gen_infinidat_reports.rb 11121 2018-07-12 23:27:42Z fma $
 
 
 require 'getoptlong'
@@ -24,7 +23,7 @@ require 'json'
 @pri_data_h = Hash.new
 @sec_data_h = Hash.new
 @data_h = Hash.new
-@site_data = { "st1" => "Dallas/Fort Worth, TX", "ev1" => "Ashburn, VA" }
+@site_data = { "st1" => "Site One", "ev1" => "Site Two" }
 
 
 def show_usage
@@ -105,7 +104,7 @@ def load_data(datafiles)
    puts "\ndatafiles: #{datafiles.inspect}" if @verbose
    datafiles.each { |df|
       read_data_h(df)
-      if not @data_h["result"].empty? then
+      if not @data_h["result"].empty?
          @data_h["result"].each { |r|
             all_data.push(r)
          }
@@ -123,12 +122,12 @@ def users(site)
    rpt_title = 'Users Report - ' + @site_data[site]
    rpt_file = @dest_path + '/'+ fn + '_' + site + '.txt'
    datafiles = Dir.glob("#{fn}_*_#{site}.json").sort
-   if not datafiles.empty? then
+   if not datafiles.empty?
       users_data = load_data(datafiles)
    else
       @mymsg.push("Critical:  Cannot find data file(s) related to #{fn}.")
    end
-   if not users_data.empty? then
+   if not users_data.empty?
       users_data.each { |u|
          u.each_pair { |k,v|
             rpt_body.push( sprintf( "%28s: %-s", k, v ) )
@@ -151,35 +150,35 @@ def config(site)
    rpt_title = 'Configuration Report - ' + @site_data[site]
    rpt_file = @dest_path + '/'+ fn + '_' + site + '.txt'
    pri_file = Dir.glob("#{fn}_*#{site}.json").pop
-   if File.exist?(pri_file) then
+   if File.exist?(pri_file)
       read_pri_data_h(pri_file)
    else
       @mymsg.push("Critical:  Cannot access #{pri_file}.")
    end
-   if not @pri_data_h["result"].empty? then
+   if not @pri_data_h["result"].empty?
       @pri_data_h["result"].each_key { |rk|
          rpt_body.push( sprintf( "%50s %-s", '', "= = = = = = = =" ) )
          rpt_body.push( sprintf( "%50s %-s", '', " #{rk} Section" ) )
          rpt_body.push( sprintf( "%50s %-s", '', "= = = = = = = =" ) )
-         if @pri_data_h["result"][rk].class == Hash then
-            if @pri_data_h["result"][rk].empty? then
+         if @pri_data_h["result"][rk].class == Hash
+            if @pri_data_h["result"][rk].empty?
                rpt_body.push( sprintf( "%50s %-s", '', 'No data available.' ) )
             else
                @pri_data_h["result"][rk].each_pair { |k,v|
-                  if v.nil? then
+                  if v.nil?
                      rpt_body.push( sprintf( "%60s : %-s", k, 'n/a' ) )
                   else
                      rpt_body.push( sprintf( "%60s : %-s", k, v ) )
                   end
                }
             end
-         elsif @pri_data_h["result"][rk].class == Array then
+         elsif @pri_data_h["result"][rk].class == Array
             @pri_data_h["result"][rk].each { |r|
                r.each_pair { |k,v|
-                  if v.nil? then
-                     rpt_body.push( sprintf( "%60s : %-s", k, 'n/a' ) )
+                  if v.nil?
+                     rpt_body.push (sprintf( "%60s : %-s", k, 'n/a' ) )
                   else
-                     rpt_body.push( sprintf( "%60s : %-s", k, v ) )
+                     rpt_body.push (sprintf( "%60s : %-s", k, v ) )
                   end
                }
             }
@@ -202,12 +201,12 @@ def pools(site)
    rpt_title = 'Pools Report - ' + @site_data[site]
    rpt_file = @dest_path + '/'+ fn + '_' + site + '.txt'
    datafiles = Dir.glob("#{fn}_*_#{site}.json").sort
-   if not datafiles.empty? then
+   if not datafiles.empty?
       pools_data = load_data(datafiles)
    else
       @mymsg.push("Critical:  Cannot find data file(s) related to #{fn}.")
    end
-   if not pools_data.empty? then
+   if not pools_data.empty?
       pools_data.each { |r|
          r.each_pair { |k,v|
             rpt_body.push( sprintf( "%38s : %-s", k, v ) )
@@ -232,15 +231,15 @@ def network_interfaces(site)
    rpt_title = 'Network Interfaces Report - ' + @site_data[site]
    rpt_file = @dest_path + '/'+ fn + '_' + site + '.txt'
    datafiles = Dir.glob("#{fn}_*_#{site}.json").sort
-   if not datafiles.empty? then
+   if not datafiles.empty?
       net_int_data = load_data(datafiles)
    else
       @mymsg.push("Critical:  Cannot find data file(s) related to #{fn}.")
    end
-   if not net_int_data.empty? then
+   if not net_int_data.empty?
       net_int_data.each { |n|
          n.each_pair { |k,v|
-            if v.nil? then
+            if v.nil?
                rpt_body.push( sprintf( "%38s : %-s", k, 'n/a' ) )
             else
                rpt_body.push( sprintf( "%38s : %-s", k, v ) )
@@ -267,14 +266,14 @@ def notifications(site)
    rpt_file = @dest_path + '/'+ fn + '_' + site + '.txt'
    [ 'targets', 'rules' ].each { |nt|
       datafiles = Dir.glob("#{fn}_#{nt}_*_#{site}.json").sort
-      if not datafiles.empty? then
+      if not datafiles.empty?
          notifications_data = load_data(datafiles)
          rpt_body.push( sprintf( "%28s %-s", '', '= = = = = = = = =' ) )
          rpt_body.push( sprintf( "%28s %-s", '', "'#{nt}' Section" ) )
          rpt_body.push( sprintf( "%28s %-s", '', '= = = = = = = = =' ) )
-         if not notifications_data.empty? then
+         if not notifications_data.empty?
             notifications_data.each { |nd|
-               if not nd.empty? then
+               if not nd.empty?
                   nd.each_pair { |k,v|
                      rpt_body.push( sprintf( "%38s : %-s", k, v ) )
                   }
@@ -308,34 +307,34 @@ def hosts_details(site)
    rpt_title = 'Hosts Details Report - ' + @site_data[site]
    rpt_file = @dest_path + '/'+ fn + '_' + site + '.txt'
    datafiles = Dir.glob("#{fn}_*_#{site}.json").sort
-   if not datafiles.empty? then
+   if not datafiles.empty?
       hosts_data = load_data(datafiles)
    else
       @mymsg.push("Critical:  Cannot find file(s) related to #{fn}.")
    end
    fn = 'cgs'
    datafiles = Dir.glob("#{fn}_*_#{site}.json").sort
-   if not datafiles.empty? then
+   if not datafiles.empty?
       cgs_data = load_data(datafiles)
    else
       @mymsg.push("Critical:  Cannot find file(s) related to #{fn}.")
    end
    fn = 'volumes'
    datafiles = Dir.glob("#{fn}_*_#{site}.json").sort
-   if not datafiles.empty? then
+   if not datafiles.empty?
       volumes_data = load_data(datafiles)
    else
       @mymsg.push("Critical:  Cannot find file(s) related to #{fn}.")
    end
-   if not hosts_data.empty? and not cgs_data.empty? and not volumes_data.empty? then
+   if not hosts_data.empty? and not cgs_data.empty? and not volumes_data.empty?
       hosts_data.each { |h|
          rpt_body.push("Name: #{h["name"]} (#{h["id"]})  #LUNs: #{h["luns"].count}")
          h["luns"].each {|l|
             volumes_data.each {|v|
-               if v["id"] == l["volume_id"] then
+               if v["id"] == l["volume_id"]
                   cgs_info = 'Unknown'
                   cgs_data.each {|c|
-                     if c["id"] == v["cg_id"] then
+                     if c["id"] == v["cg_id"]
                         cgs_info = c["name"] + " (" + c["id"].to_s + ")"
                      end
                   }
@@ -367,24 +366,24 @@ def cgs_volumes(site)
    fn = 'cgs'
    rpt_file = @dest_path + '/'+ fn + '_' + site + '.txt'
    datafiles = Dir.glob("#{fn}_*_#{site}.json").sort
-   if not datafiles.empty? then
+   if not datafiles.empty?
       cgs_data = load_data(datafiles)
    else
       @mymsg.push("Critical:  Cannot find file(s) related to #{fn}.")
    end
    fn = 'volumes'
    datafiles = Dir.glob("#{fn}_*_#{site}.json").sort
-   if not datafiles.empty? then
+   if not datafiles.empty?
       volumes_data = load_data(datafiles)
    else
       @mymsg.push("Critical:  Cannot find file(s) related to #{fn}.")
    end
-   if not cgs_data.empty? and not volumes_data.empty? then
+   if not cgs_data.empty? and not volumes_data.empty?
       cgs_data.each { |c|
          rpt_body.push("Name: #{c["name"]} (#{c["id"]})  #Members: \
 #{c["members_count"]}  Replicated? #{c["is_replicated"]}  Type: #{c["type"]}")
          volumes_data.each { |v|
-            if v["cg_id"] == c["id"] then
+            if v["cg_id"] == c["id"]
                rpt_body.push("\t#{v["name"]} (#{v["id"]})  Size: \
 #{v["size"]/1024**3} GiB  Used: #{v["used"]/1024**3} GiB")
             end
@@ -411,12 +410,12 @@ def network_spaces(site)
    rpt_title = 'Network Spaces Report - ' + @site_data[site]
    rpt_file = @dest_path + '/'+ fn + '_' + site + '.txt'
    datafiles = Dir.glob("#{fn}_*_#{site}.json").sort
-   if not datafiles.empty? then
+   if not datafiles.empty?
       net_spc_data = load_data(datafiles)
    else
       @mymsg.push("Critical:  Cannot find file(s) related to #{fn}.")
    end
-   if not net_spc_data.empty? then
+   if not net_spc_data.empty?
       net_spc_data.each { |n|
          n.each_pair { |k,v|
             mykey = k
@@ -432,7 +431,7 @@ def network_spaces(site)
                      mykey = ''
                   }
                else
-                  if not v.nil? then
+                  if not v.nil?
                      rpt_body.push( sprintf( "%28s : %-s", k, v ) )
                   else
                      rpt_body.push( sprintf( "%28s : %-s", k, 'n/a' ) )
@@ -460,51 +459,51 @@ def san_clients(site)
    rpt_title = 'SAN Clients Report - ' + @site_data[site]
    rpt_file = @dest_path + '/'+ fn + '_' + site + '.txt'
    datafiles = Dir.glob("#{fn}_*_#{site}.json").sort
-   if not datafiles.empty? then
+   if not datafiles.empty?
       san_clients_data = load_data(datafiles)
    else
       @mymsg.push("Critical:  Cannot find file(s) related to #{fn}.")
    end
    fn = 'volumes'
    datafiles = Dir.glob("#{fn}_*_#{site}.json").sort
-   if not datafiles.empty? then
+   if not datafiles.empty?
       volumes_data = load_data(datafiles)
    else
       @mymsg.push("Critical:  Cannot find file(s) related to #{fn}.")
    end
-   if not san_clients_data.empty? and not volumes_data.empty? then
+   if not san_clients_data.empty? and not volumes_data.empty?
       san_clients_data.each { |s|
          s.each_pair { |k,v|
             mykey = k
             case v.class.to_s
                when 'Array'
                   v.each { |vv|
-                     if k == 'luns' then
+                     if k == 'luns'
                         volumes_data.each { |vd|
-                           if vd["id"] == vv["volume_id"] then
+                           if vd["id"] == vv["volume_id"]
                               rpt_body.push( sprintf( "%38s : %-s (%-s)  Size: %-s GB", \
 mykey, vd["name"] , vd["id"], vd["size"]/1024**3 ) )
                            end
                         }
-                     elsif k == 'hosts' then
+                     elsif k == 'hosts'
                         mykey = k
                         vv.each_pair { |vvk,vvv|
-                           if vvk == 'luns' then
+                           if vvk == 'luns'
                               vvv.each { |vvvv|
                                  volumes_data.each { |vd|
-                                    if vd["id"] == vvvv["volume_id"] then
+                                    if vd["id"] == vvvv["volume_id"]
                                        rpt_body.push( sprintf( "%38s : luns : %-s (%-s)  \
    Size: %-s GB", mykey, vd["name"], vd["id"], vd["size"]/1024**3 ) )
                                     end
                                  }
                               }
-                           elsif vvk == 'ports' then
+                           elsif vvk == 'ports'
                               vvv.each { |vvvv|
                                  rpt_body.push( sprintf( "%38s : ports : %-s", \
 mykey, vvvv ) )
                               }
                            else
-                              if vvv.nil? then
+                              if vvv.nil?
                                  rpt_body.push( sprintf( "%38s : %-s : %-s", \
 mykey, vvk, 'n/a' ) )
                               else
@@ -524,7 +523,7 @@ mykey, vvk, vvv ) )
                      rpt_body.push( sprintf( "%38s : %-s : %-s", mykey, vk, vv ) )
                   }
                else
-                  if not v.nil? then
+                  if not v.nil?
                      rpt_body.push( sprintf( "%38s : %-s", k, v ) )
                   else
                      rpt_body.push( sprintf( "%38s : %-s", k, 'n/a' ) )
@@ -552,15 +551,15 @@ def filesystems(site)
    rpt_title = 'File Systems Report - ' + @site_data[site]
    rpt_file = @dest_path + '/'+ fn + '_' + site + '.txt'
    datafiles = Dir.glob("#{fn}_*_#{site}.json").sort
-   if not datafiles.empty? then
+   if not datafiles.empty?
       filesystems_data = load_data(datafiles)
    else
       @mymsg.push("Critical:  Cannot find file(s) related to #{fn}.")
    end
-   if not filesystems_data.empty? then
+   if not filesystems_data.empty?
       filesystems_data.each { |f|
          f.each_pair { |k,v|
-            if v.nil? then
+            if v.nil?
                rpt_body.push( sprintf( "%38s : %-s", k, 'n/a' ) )
             else
                rpt_body.push( sprintf( "%38s : %-s", k, v ) )
@@ -586,16 +585,16 @@ def services(site)
    rpt_title = 'Services Report - ' + @site_data[site]
    rpt_file = @dest_path + '/'+ fn + '_' + site + '.txt'
    datafiles = Dir.glob("#{fn}_*_#{site}.json").sort
-   if not datafiles.empty? then
+   if not datafiles.empty?
       services_data = load_data(datafiles)
    else
       @mymsg.push("Critical:  Cannot find file(s) related to #{fn}.")
    end
-   if not services_data.empty? then
+   if not services_data.empty?
       services_data.each { |s|
          s.each_pair { |k,v|
             mykey = k
-            if v.class == Array then
+            if v.class == Array
                v.each { |vv|
                   vv.each_pair { |vvk,vvv|
                      rpt_body.push( sprintf( "%28s : %-8s : %-s", mykey, vvk, vvv ) )
@@ -629,15 +628,21 @@ def events(site)
       fn = 'events' + '_' + l
       datafiles = Dir.glob("#{fn}_*_#{site}.json").sort
       rpt_body.push( sprintf( "%28s %s %s\n", '= = =', "#{l.upcase} Events", '= = =' ) )
-      if not datafiles.empty? then
+      if not datafiles.empty?
          events_data = load_data(datafiles)
       else
          @mymsg.push("Critical:  Cannot find file(s) related to #{fn}.")
       end
-      if not events_data.empty? then
+      if not events_data.empty?
          for i in 0..(num - 1)
             events_data[i].each_pair { |k,v|
-               rpt_body.push( sprintf( "%28s : %s\n", k, v ) ) if k !~ /template/
+               if k !~ /template/ and k !~ /system_version/
+                  if k == 'timestamp'
+                     rpt_body.push( sprintf( "%28s : %s\n", k, Time.at( v.to_s.slice(0,10).to_i ) ) )
+                  else
+                     rpt_body.push( sprintf( "%28s : %s\n", k, v ) )
+                  end
+               end
             }
             rpt_body.push( sprintf( "%23s %s", '', '- - - - - - - -' ) )
          end
@@ -664,12 +669,12 @@ end
 #   rpt_file = @dest_path + '/'+ fn + '_' + site + '.txt'
 #   pri_file = @src_path + '/' + fn + '_' + site + '.json'
 #   datafiles = Dir.glob("#{fn}_*_#{site}.json").sort
-#   if not datafiles.empty? then
+#   if not datafiles.empty?
 #      {blah}_data = load_data(datafiles)
 #   else
 #      @mymsg.push("Critical:  Cannot find file(s) related to #{fn}.")
 #   end
-#   if not {blah}_data.empty? then
+#   if not {blah}_data.empty?
 #      {blah}_data.each { |x|
 #      }
 #   else
@@ -682,7 +687,7 @@ end
 #end
 
 # verify source and destination directories
-if File.directory?("#{@src_path}") and File.directory?("#{@dest_path}") then
+if File.directory?("#{@src_path}") and File.directory?("#{@dest_path}")
    puts 'Directories verified' if @verbose
    Dir.chdir(@src_path)
    @mymsg.clear
