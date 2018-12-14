9133cb32c367e5b37ad4b9a54ea646b090dd9938
Francis Ma <bzbaba@outlook.com>
2018-12-14 09:23:14 -0800

    wip

--- MyCloud/mc_azure.py
+++ MyCloud/mc_azure.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+import pprint
+from azure.mgmt.network import NetworkManagementClient
+from azure.mgmt.compute import ComputeManagementClient
+from azure.mgmt.managementgroups import operations
+from azure.common.client_factory import get_client_from_cli_profile
+
+
+def GetCompute():
+   client = get_client_from_cli_profile(ComputeManagementClient)
+
+   pprint.pprint(client)
+
+def GetResourceGroup():
+   resource_group = 
+
+# for testing purposes
+if __name__ == "__main__":
+   GetResourceGroup()
