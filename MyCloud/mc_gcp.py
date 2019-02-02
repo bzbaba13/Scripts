4f59555b593e094bcf3f7baa34620ae3965a52fd
Francis Ma <mafbaba@gmail.com>
2019-02-02 09:44:41 -0800

    starting work on GCP

--- MyCloud/mc_gcp.py
+++ MyCloud/mc_gcp.py
@@ -1,1 +1,6 @@
-#!/usr/bin/env python3
+# This is the GCP portion of the MyCloud application.
+# Python 3.6+, GCloud SDK, and  google-api-python-client
+# are required for execution.
+
+import googleapiclient
+import json, pprint
