c5e5ceedf9bd2ebcfb5e669010bb693ed0e326ad
Francis Ma <bzbaba@outlook.com>
2022-10-02 17:34:48 -0700

    minor modifications

--- MyCloud/mc_gcp.py
+++ MyCloud/mc_gcp.py
@@ -4,3 +4,10 @@
 
 import googleapiclient
 import json, pprint
+
+
+compute = googleapiclient.discovery.build('compute', 'v1')
+
+def list_instances(compute, project, zone):
+    result = compute.instances().list(project=project, zone=zone).execute()
+    return result['items'] if 'items' in result else None
